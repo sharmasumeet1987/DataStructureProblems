f697360b652d26ce547df407efc0c4deef1ab51a
Sumeet Sharma <sharmasumeet1987@gmail.com>
2020-03-07 17:53:30 -0500

    Print squares till n

---
+++ DataStructures/Arrays/Problems.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    class Problems
+    {
+        // Print squares till n
+        public static int PrintSquares(int n)
+        {
+            if (n == 1) {
+                Console.Write($"{n} ");
+                return 1; }
+            else
+            {
+                int prev = PrintSquares(n / 2);
+                int curr = 2 * prev;
+                Console.Write($"{curr} ");
+                return curr;
+            }
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructures.Arrays;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            Problems.PrintSquares(1000000);
+            Console.ReadKey();
         }
     }
 }
